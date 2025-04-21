6a327745a68c76b1b09ef1e7876c189c9f5d427b
Antoine KLEIN <antoineklein2000@gmail.com>
2025-04-21 22:41:17 +0200

    fix color scheme adjustments

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,14 +1,19 @@
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "*",
+  callback = function()
+    vim.api.nvim_set_hl(0, "Function", { fg = "#DCDCAA" }) -- yellowish like VSCode
+    vim.api.nvim_set_hl(0, "@type", { fg = "#4EC9B0" }) -- tealish
+    vim.api.nvim_set_hl(0, "@variable", { fg = "#9CDCFE" }) -- blue
+    vim.api.nvim_set_hl(0, "@keyword", { fg = "#C586C0" }) -- purple
+    vim.api.nvim_set_hl(0, "@string", { fg = "#CE9178" }) -- orange
+  end,
+})
 return {
   {
     "martinsione/darkplus.nvim",
     config = function()
       vim.cmd("colorscheme darkplus")
-
       -- Example tweaks (adjust as needed)
-      vim.api.nvim_set_hl(0, "@function", { fg = "#DCDCAA" }) -- yellowish like VSCode
-      vim.api.nvim_set_hl(0, "@type", { fg = "#4EC9B0" }) -- tealish
-      vim.api.nvim_set_hl(0, "@variable", { fg = "#9CDCFE" }) -- blue
-      vim.api.nvim_set_hl(0, "@keyword", { fg = "#C586C0" }) -- purple
     end,
   },
 }
